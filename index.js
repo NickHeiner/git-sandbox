11474e8dc88043aa2086de916ccfb4cacc8bd4d6
Nick Heiner <nheiner@netflix.com>
2020-08-24 14:13:29 -0400

    Create initial file.

---
+++ index.js
@@ -0,0 +1,26 @@
+// Additional entry will be added to the end of the function.
+function f(
+  a,
+  b,
+  c
+) {
+
+}
+
+// Last entry will be renamed.
+function f2(
+  a,
+  b,
+  c
+) {
+
+}
+
+// Last entry will be removed.
+function f3(
+  a,
+  b,
+  c
+) {
+
+}
